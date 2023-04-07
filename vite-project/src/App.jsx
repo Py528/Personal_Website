2464cfcc6e58db6ea2dc4433e961f127d6ca511d
Py528 <pranaav.shinde5280@gmail.com>
2023-04-07 13:40:32 +0530

    intial setup completed

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="bg-gray-200 p-4 m-40 flex justify-center items-center">
+				<h1 className="text-3xl font-bold text-green-800 center">Hello, Tailwind CSS!</h1>
+				<button className="btn btn-outline">Button</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
