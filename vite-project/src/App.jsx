85abf3b23e349190032e609a50b45b5a8113dc0f
Py528 <pranaav.shinde5280@gmail.com>
2023-04-07 23:35:10 +0530

    Navbar Intro component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
+import React, { ReactDOM } from 'react';
+import Navbar from './components/NavBar';
+import Intro from './components/Intro';
 
 function App() {
 	return (
 		<div className="App">
-			<div className="bg-gray-200 p-4 m-40 flex justify-center items-center">
-				<h1 className="text-3xl font-bold text-green-800 center">Hello, Tailwind CSS!</h1>
-				<button className="btn btn-outline">Button</button>
-			</div>
+			<Navbar />
+			<Intro />
 		</div>
 	);
 }
