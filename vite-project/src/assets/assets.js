fe936796d65278955dff19ea90b12b77199253a1
Py528 <pranaav.shinde5280@gmail.com>
2023-04-08 22:11:38 +0530

    exported the assets

---
+++ vite-project/src/assets/assets.js
@@ -0,0 +1,11 @@
+export { default as wave } from './waving.1bae5fcfb51082b5c2b4.png';
+export { default as LinkedIn } from './linkedin-icon.svg';
+export { default as Github } from './github-icon-1.svg';
+export { default as Git } from './git-icon.svg';
+export { default as Tailwind } from './tailwind-css-2.svg';
+export { default as CSS } from './css-3.svg';
+export { default as JavaScript } from './logo-javascript.svg';
+export { default as HTML } from './html-1.svg';
+export { default as NODE } from './nodejs-icon.svg';
+export { default as Reacts } from './react-2.svg';
+export { default as Personalphoto } from './personal_photo.jpeg';
