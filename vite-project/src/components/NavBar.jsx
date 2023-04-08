c979be3b975c36ab726dd0682a1a714c31b0877d
Py528 <pranaav.shinde5280@gmail.com>
2023-04-08 22:14:05 +0530

    modified stylings and added new links

--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -1,53 +1,52 @@
 import React from 'react';
 function NavBar() {
 	return (
-		<div className="sticky top-0">
+		<div className="sticky top-0 bg-white shadow-md text-gray-800 h-22 flex-1 items-center justify-center px-8">
 			<nav
 				aria-label="Site Nav"
-				className="mx-auto flex items-center justify-between p-4 max-w-full "
+				class="mx-auto flex items-center justify-between p-4 max-w-full h-full"
 			>
 				<a
 					href="/"
-					className="flex items-center justify-center"
+					class="flex items-center justify-center"
 				>
-					<span className="text-3xl font-bold">Pranav Shinde</span>
+					<span class="text-2xl font-bold">Pranav.Shinde</span>
 				</a>
-
-				<ul className="flex items-center gap-3 text-2xl font-medium">
-					<li className="hidden lg:block">
+				<ul class="flex items-center gap-2 text-xl font-semibold justify-center">
+					<li class="hidden lg:block">
 						<a
-							className="rounded-lg px-3 py-2"
+							class="rounded-lg px-3 py-2"
 							href="/"
 						>
 							{' '}
 							Home{' '}
 						</a>
 					</li>
-
 					<li>
 						<a
-							className="rounded-lg px-3 py-2"
+							class="rounded-lg px-3 py-2"
+							href=""
+						>
+							{' '}
+							About{' '}
+						</a>
+					</li>
+					<li>
+						<a
+							class="rounded-lg px-3 py-2"
 							href=""
 						>
 							{' '}
 							Projects{' '}
 						</a>
 					</li>
-
 					<li>
 						<a
-							className="inline-flex items-center gap-2 rounded-lg px-3 py-2"
+							class="rounded-lg px-3 py-2"
 							href=""
-							target="_blank"
 						>
-							External
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								className="text-2xl"
-							></svg>
+							{' '}
+							Contact{' '}
 						</a>
 					</li>
 				</ul>
