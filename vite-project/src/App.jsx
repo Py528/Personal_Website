36ca211bf141af5e03fd565a416f4538b3b6b935
Py528 <pranaav.shinde5280@gmail.com>
2023-04-09 10:19:43 +0530

    added About component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import React, { ReactDOM } from 'react';
 import Navbar from './components/NavBar';
 import Intro from './components/Intro';
+import About from './components/About';
 
 function App() {
 	return (
 		<div className="App bg-neutral-50 h-screen">
 			<Navbar />
 			<Intro />
+			<About />
 		</div>
 	);
 }
