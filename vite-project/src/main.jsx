4de31d000ea02fadb84aa7f3cd651d67855d9283
Py528 <pranaav.shinde5280@gmail.com>
2023-04-16 23:19:58 +0530

    added Projects section

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -5,11 +5,13 @@ import NavBar from './components/NavBar';
 import About from './components/About';
 import './index.css';
 import './App.css';
+import Projects from './components/Projects';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-		<React.StrictMode>
-			<NavBar />
-			<Home />
-			<About />
-		</React.StrictMode>
+	<React.StrictMode>
+		<NavBar />
+		<Home />
+		<About />
+		<Projects />
+	</React.StrictMode>
 );
