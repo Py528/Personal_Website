9eae7a3f886390e35731e1c11fb21ec8833684c7
Py528 <pranaav.shinde5280@gmail.com>
2023-04-16 10:27:33 +0530

    version 1 of about us

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -4,17 +4,21 @@ import { Macbook } from '../assets/assets';
 function About() {
 	return (
 		<div className="about m-auto w-[100%] p-[5rem] bg-white">
-			<div className="container">
+			<div className="container mx-auto w-[1200px] flex gap-[5rem] justify-center items-center">
 				<div className="about-content flex justify-center items-center">
 					<div className="img-side min-w-[50%] min-h-[50%]">
-						<img src={Macbook} />
+						<img
+							src={Macbook}
+							className="rounded-[16px]"
+							alt="macbook"
+						/>
 					</div>
 					<div className="text-side text-[1.2rem] leading-[1.5rem] mt-[1.5rem] mb-[2.5rem] lg:m p-[2rem]">
-						<h2>About me</h2>
-						<h3>
+						<h2 className="text-[#008cff] font-bold leading-[2rem] mb-[1.5rem]">About me</h2>
+						<h3 className="text-[#333] text-[2rem] leading-[2rem] my-[1.5rem] font-bold">
 							A dedicated Front-end Developer <br /> based in Pune, India 📍
 						</h3>
-						<p>
+						<p className="text-[#333] leading-[1.5rem] text-[1rem] mb-[2rem]">
 							As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS,
 							JavaScript, React, Tailwind, and SCSS. I excel in designing and maintaining responsive
 							websites that offer a smooth user experience. My expertise lies in crafting dynamic,
@@ -28,5 +32,5 @@ function About() {
 		</div>
 	);
 }
-
+// acics
 export default About;
