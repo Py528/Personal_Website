47b97c8527f8e367ef4bcb479c0555ca287eef36
Py528 <pranaav.shinde5280@gmail.com>
2023-05-01 00:34:21 +0530

    added zig zag pattern

--- vite-project/src/components/Projects.jsx
+++ vite-project/src/components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Project1 from './Project1';
+import Project2 from './Project2';
+import Project3 from './Project3';
+import Project4 from './Project4';
 function Projects() {
 	return (
 		<section className="project bg-gray-100 max-w-full">
@@ -12,9 +15,9 @@ function Projects() {
 				</div>
 				<div class="projects-grid flex flex-col gap-16 my-16">
 					<Project1 />
-					<Project1 />
-					<Project1 />
-					<Project1 />
+					<Project2 />
+					<Project3 />
+					<Project4 />
 				</div>
 			</div>
 		</section>
